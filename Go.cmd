4549837a831ccd381824ee67ccaafaf65c06a572
Kleinas Emilis <emilis.kleinas@mif.stud.vu.lt>
2024-10-08 16:08:20 +0300

    Improved Go.cmd

--- Go.cmd
+++ Go.cmd
@@ -1,6 +1,11 @@
-set PATH=%PATH%;
-mvn compile test
-mvn exec:java -D exec.mainClass="Main" -D exec.args="input_sample.txt input_sample__out_good.md input_sample__out_bad.txt"
-mvn exec:java -D exec.mainClass="Main" -D exec.args="input_new_rules.txt input_new_rules__out_good.md input_new_rules__out_bad.txt"
-mvn exec:java -D exec.mainClass="Main" -D exec.args="input_bad.txt input_bad__out_good.md input_bad__out_bad.txt"
+@echo off
+echo Compiling code...
+CALL mvn compile
 
+echo Running tests...
+CALL mvn test
+
+echo Executing program on sample files...
+CALL mvn exec:java -D exec.mainClass="Main" -D exec.args="input_sample.txt input_sample__out_good.md input_sample__out_bad.txt"
+CALL mvn exec:java -D exec.mainClass="Main" -D exec.args="input_new_rules.txt input_new_rules__out_good.md input_new_rules__out_bad.txt"
+CALL mvn exec:java -D exec.mainClass="Main" -D exec.args="input_bad.txt input_bad__out_good.md input_bad__out_bad.txt"
