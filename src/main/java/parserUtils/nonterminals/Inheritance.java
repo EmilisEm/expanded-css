7a09ef2a2c65f6aaab9676d1531e04d629fd6bb7
Kleinas Emilis <emilis.kleinas@mif.stud.vu.lt>
2024-10-26 20:37:15 +0300

    Added Inheritance class

---
+++ src/main/java/parserUtils/nonterminals/Inheritance.java
@@ -0,0 +1,14 @@
+package parserUtils.nonterminals;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Inheritance {
+    private List<String> inherited;
+}
