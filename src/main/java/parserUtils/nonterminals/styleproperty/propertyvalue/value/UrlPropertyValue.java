8fbfac40f36a06712ae79d18278aff430bd7226d
Kleinas Emilis <emilis.kleinas@mif.stud.vu.lt>
2024-10-29 09:06:51 +0200

    Added url property value

---
+++ src/main/java/parserUtils/nonterminals/styleproperty/propertyvalue/value/UrlPropertyValue.java
@@ -0,0 +1,12 @@
+package parserUtils.nonterminals.styleproperty.propertyvalue.value;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UrlPropertyValue extends PropertyValue {
+    private String value;
+}
