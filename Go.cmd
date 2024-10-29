3c6480e78ff1e2c82c1c2214ffff8380424a878c
Kleinas Emilis <emilis.kleinas@mif.stud.vu.lt>
2024-10-29 17:00:32 +0200

    Added extended go file

--- Go.cmd
+++ Go.cmd
@@ -8,4 +8,4 @@ CALL mvn test
 echo Executing program on sample files...
 CALL mvn exec:java -D exec.mainClass="Main" -D exec.args="input_sample.txt input_sample__out_good.md input_sample__out_bad.txt"
 CALL mvn exec:java -D exec.mainClass="Main" -D exec.args="input_new_rules.txt input_new_rules__out_good.md input_new_rules__out_bad.txt"
-CALL mvn exec:java -D exec.mainClass="Main" -D exec.args="input_bad.txt input_bad__out_good.md input_bad__out_bad.txt"
+CALL mvn exec:java -D exec.mainClass="Main" -D exec.args="input_bad_parser.txt input_bad__out_good.md input_bad__out_bad.txt"
